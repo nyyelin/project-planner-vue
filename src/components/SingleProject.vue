47c336cd8b9662e495a1d1ed230951678be596d7
nyyelin <nyiyelin4@gmail.com>
2022-10-27 00:34:36 +0630

    first commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="project">
+    <div class="flexing">
+      <div>
+        <h3 @click="showDetailToogle">{{ project.title }}</h3>
+      </div>
+      <div>
+        <span class="material-icons" @click="deleteProject"> delete </span>
+        <span class="material-icons"> edit </span>
+        <span class="material-icons"> done </span>
+      </div>
+      
+    </div>
+    <p v-if="detail === true">{{ project.detail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      detail: false,
+    };
+  },
+
+  methods: {
+    showDetailToogle() {
+        
+      this.detail = !this.detail;
+    },
+  },
+};
+</script>
+
+<style>
+.project {
+  padding: 20px;
+  background-color: white;
+  margin: 14px 10px;
+  box-shadow: 4px 5px 20px rgb(180, 179, 179);
+  border-radius: 5px;
+  border-left: 5px solid crimson;
+}
+h3 {
+  color: indigo;
+  cursor: pointer;
+}
+.flexing {
+  display: flex;
+  justify-content: space-between;
+  align-items: center; /** vertical **/
+}
+span{
+    margin-left: 15px;
+    cursor: pointer;
+}
+span:hover{
+    color: #777;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  data(){
+    return {
+      projects: [],
+    }
+  },
   components: {
-    HelloWorld
+    SingleProject,
+  },
+  mounted(){
+    fetch("http://localhost:3000/projects").then((response) => {
+      return response.json()
+    }).then((datas) => {
+      this.projects = datas
+    }).catch((err) => {
+      console.log(err.message())
+    })
   }
 }
 </script>
